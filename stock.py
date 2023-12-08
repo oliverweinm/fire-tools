a96f4aa458545febe6d620fe9c0fcf5dc73f3a5d
oliverweinm <oliver.weinmeier@unibas.ch>
2023-12-08 11:01:29 +0100

    Add Stock class

---
+++ stock.py
@@ -0,0 +1,21 @@
+class Stock:
+	def __init__(self, name, price, amount, dividend_per_share, div_freq, isin, annual_divs=None, div_yield=None, total_holdings=None):
+		self.name = name
+		self.price = price
+		self.amount = amount
+		self.dividend_per_share = dividend_per_share
+		self.div_freq = div_freq
+		self.isin = isin
+		self.annual_divs = self.amount * self.dividend_per_share
+		self.div_yield = (self.dividend_per_share / self.price)*100
+		self.total_holdings = self.price * self.amount
+	def calc_div_freq():
+		return NotImplementedError
+	def __str__(self):
+		return f"{self.name}({self.isin}):\n{self.amount}x shares at {self.price}€ -> Total holdings: {self.total_holdings}€\n\
+		{self.dividend_per_share}€ dividend per share -> Annual dividend: {self.annual_divs})"
+
+
+if __name__ == "__main__":
+	apple = Stock("apple",175,3,0.91,4,8888)
+	print(apple)
